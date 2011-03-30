82a4a3e5b877497723e441d11d01ddcb9ae36372
sam.ratcliff@gmail.com <sam.ratcliff@gmail.com@f7df9462-281d-0e4b-85b5-66809da897f8>
2011-03-30 09:49:35 +0000

    update in line with documentation

---
+++ trunk/Software/my_software/src/Core/CircuitEvolution/QPace4/QPace4_Local.java
@@ -0,0 +1,54 @@
+/**
+ * @Author = Sam Ratcliff
+ */
+package Core.CircuitEvolution.QPace4;
+
+import Core.CircuitEvolution.SearchEngineState;
+import Utils.SendMail;
+import ec.util.ParameterDatabase;
+
+public class QPace4_Local extends QPace4_Imp {
+
+	/**
+	 * 
+	 */
+	public QPace4_Local() {
+		super();
+	}
+
+	@Override
+	protected void startSearch() {
+		resetProgressBar();
+		updateState(SearchEngineState.Searching);
+		String[] pstr = { "-file", filename };
+		final ParameterDatabase pd = QPaceSearchCore
+				.loadParameterDatabase(pstr);
+		searchres = new QPaceSearchResult[iterval];
+		final String probname = this.ce.getQproblem().getName();
+		final QPaceSearchCore[] task_array = new QPaceSearchCore[iterval];
+		final int geners = gen;
+
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < iterval; i++) {
+					task_array[i] = new QPaceSearchCore(pd, cb, ce,
+							(StatsPanel) getSearchStatisticsPanel());
+					task_array[i].run();
+					searchres[i] = (QPaceSearchResult) task_array[i]
+							.getResult();
+					((StatsPanel) getSearchStatisticsPanel()).getProgressBar()
+							.setValue((i + 1) * geners);
+				}
+				updateState(SearchEngineState.SearchCompleteResultAvailable);
+				if (!to.equalsIgnoreCase("")) {
+					SendMail sendMail = new SendMail(from, to, subject, message
+							.concat(probname));
+					sendMail.send();
+				}
+			}
+		});
+		t.start();
+	}
+}
