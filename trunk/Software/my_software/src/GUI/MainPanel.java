8001d262e0fd18bff02ef305eb82e2f7a31859a4
sam.ratcliff@gmail.com <sam.ratcliff@gmail.com@f7df9462-281d-0e4b-85b5-66809da897f8>
2011-05-09 12:50:38 +0000

    Final software changes

--- trunk/Software/my_software/src/GUI/MainPanel.java
+++ trunk/Software/my_software/src/GUI/MainPanel.java
@@ -19,15 +19,19 @@ import GUI.SubPanels.RPanel;
  * 
  */
 public class MainPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long			serialVersionUID	= 8605194316843038889L;
 	private final JPanel				left_panel;
 	private final JPanel				centre_panel;
 	private final JPanel				right_panel;
 	private final Dimension				screen_size;
-	public static final double			left_perc		= 0.2;
-	public static final double			centre_perc		= 0.59;
-	public static final double			right_perc		= 0.2;
-	public static final double			height_perc		= 0.95;
-	public static final int				titleFontSize	= 25;
+	public static final double			left_perc			= 0.2;
+	public static final double			centre_perc			= 0.59;
+	public static final double			right_perc			= 0.2;
+	public static final double			height_perc			= 0.95;
+	public static final int				titleFontSize		= 25;
 	private final qcevolutionbackend	backend;
 
 	/**
@@ -44,22 +48,25 @@ public class MainPanel extends JPanel {
 		centre_panel.setBorder(BorderFactory.createEtchedBorder());
 		right_panel.setBorder(BorderFactory.createEtchedBorder());
 
+		left_panel.setPreferredSize(new Dimension(
+				(int) (screen_size.width * left_perc),
+				(int) (screen_size.height * height_perc)));
+		centre_panel.setPreferredSize(new Dimension(
+				(int) (screen_size.width * centre_perc),
+				(int) (screen_size.height * height_perc)));
+		right_panel.setPreferredSize(new Dimension(
+				(int) (screen_size.width * right_perc),
+				(int) (screen_size.height * height_perc)));
 
-		left_panel.setPreferredSize(new Dimension((int) (screen_size.width * left_perc),
+		left_panel.setMaximumSize(new Dimension(
+				(int) (screen_size.width * left_perc),
 				(int) (screen_size.height * height_perc)));
-		centre_panel.setPreferredSize(new Dimension((int) (screen_size.width * centre_perc),
+		centre_panel.setMaximumSize(new Dimension(
+				(int) (screen_size.width * centre_perc),
 				(int) (screen_size.height * height_perc)));
-		right_panel.setPreferredSize(new Dimension((int) (screen_size.width * right_perc),
+		right_panel.setMaximumSize(new Dimension(
+				(int) (screen_size.width * right_perc),
 				(int) (screen_size.height * height_perc)));
-
-		left_panel.setMaximumSize(new Dimension((int) (screen_size.width * left_perc),
-				(int) (screen_size.height * height_perc)));
-		centre_panel.setMaximumSize(new Dimension((int) (screen_size.width * centre_perc),
-				(int) (screen_size.height * height_perc)));
-		right_panel.setMaximumSize(new Dimension((int) (screen_size.width * right_perc),
-				(int) (screen_size.height * height_perc)));
-		
-		
 
 		this.add(left_panel, BorderLayout.LINE_START);
 		this.add(centre_panel, BorderLayout.CENTER);
